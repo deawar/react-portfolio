e18e839747dc540c3374f3f0b590ec26e383de03
Dean Warren <deawar@gmail.com>
2020-07-27 00:13:07 -0400

    updated Routes

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -3,10 +3,16 @@ import React from "react";
 function Jumbotron({ children }) {
   return (
     <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
+      style={{ height: 300, clear: "both", textAlign: "center" }}
       className="jumbotron"
     >
-      {children}
+      {/* {children} */}
+      <div className="view overlay my-4">
+        <img src="https://mdbootstrap.com/img/Photos/Slides/img%20(70).jpg" class="img-fluid" alt="Coastal Image"/>
+        <a href="/">
+        <div className="mask rgba-white-slight"></div>
+        </a>
+      </div>
     </div>
   );
 }
--- src/components/Nav/NavBarMenu.jsx
+++ src/components/Nav/NavBarMenu.jsx
@@ -4,21 +4,27 @@ import {Row} from 'react-materialize';
 import './SocialMedia';
 import Socialmedia from './SocialMedia';
 import './NavBarMenu.css';
+import { Link, useLocation } from "react-router-dom";
 
 export default function NavBarMenu() {
+    const location = useLocation();
     return (
         <Row className="nav-wrapper">
-            <a className="navbar-brand responsive-img" target="_blank" rel="noopener noreferrer" href="https://github.com/deawar" alt="Site Logo-Dean Warren"></a>
+            <a className="navbar-brand responsive-img" target="_blank" rel="noopener noreferrer" href="https://github.com/deawar" alt="Github Repo Link"></a>
             <div class="hide-on-med-and-up">
-                {/* <SideNavMenu /> */}
-                <a href="#" Data-activate="mobile-demo" class="col s11 button-collapse" alt="Hamburger Menu"/><i class="material-icons">menu</i><a/>
+                <a href="#about" Data-activate="mobile-demo" class="col s11 button-collapse" alt="Hamburger Menu"/><i class="material-icons">menu</i><a/>
             </div>
         
         <a id="logo-container" href="#footer" className="col s6 m12 l3 center brand-logo">Dean E. Warren</a> 
             <ul class="nav-right col s6 m12 l9 hide-on-med-and-down">
                 <Socialmedia />
                 <li className="NavItem">
-                    <a href="#about" className="nav-link">About</a>
+                    <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
+                        Home
+                    </Link>
+                </li>
+                <li className="NavItem">
+                    <a href="/about" className="nav-link">About</a>
                 </li>
                 <li clasNames="NavItem">
                     <a href="#portfolio" className="nav-link">Portfolio</a>
@@ -26,6 +32,15 @@ export default function NavBarMenu() {
                 <li className="NavItem">
                     <a href="#footer" className="nav-link">Contact</a>
                 </li>
+                <li className="NavItem">
+                    <Link
+                        to="/blog"
+                        className={location.pathname === "/blog" ? "nav-link active" : "nav-link"}
+                    >
+                        Blog
+                    </Link>
+                </li>
+
             </ul>
         </Row>
     )
