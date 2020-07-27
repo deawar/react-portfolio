62cf97deebf09bcb60935b009f51f572aa7e17d1
Dean Warren <deawar@gmail.com>
2020-07-27 00:16:14 -0400

    Updates

---
+++ precache-manifest.98f98b9886636ae385475625bd8e6f63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f1d4b3838f8a5d8a7a82b4c568c9829",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "5d92d37c6ec57397c7a8",
+    "url": "/react-portfolio/static/css/2.199796d5.chunk.css"
+  },
+  {
+    "revision": "7aa903a76d73fda58ec2",
+    "url": "/react-portfolio/static/css/main.487e50b2.chunk.css"
+  },
+  {
+    "revision": "5d92d37c6ec57397c7a8",
+    "url": "/react-portfolio/static/js/2.a9661088.chunk.js"
+  },
+  {
+    "revision": "8debe1713c30c1c8fa79b57451984727",
+    "url": "/react-portfolio/static/js/2.a9661088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aa903a76d73fda58ec2",
+    "url": "/react-portfolio/static/js/main.1a616edf.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  }
+]);
